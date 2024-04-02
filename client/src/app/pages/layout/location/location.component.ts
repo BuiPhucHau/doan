bbe88585cfc2409713912619cc3dd75d7451de4c
Hau <hau.bp12201@sinhvien.hoasen.edu.vn>
2024-04-02 17:59:40 +0700

    a

--- client/src/app/pages/layout/location/location.component.ts
+++ client/src/app/pages/layout/location/location.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ShareModule } from '../../../shared/shared.module';
+import { TaigaModule } from '../../../shared/taiga.module';
 
 @Component({
   selector: 'app-location',
   standalone: true,
-  imports: [],
+  imports: [ShareModule, TaigaModule],
   templateUrl: './location.component.html',
   styleUrl: './location.component.scss'
 })
