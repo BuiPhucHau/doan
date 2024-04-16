f9f96bd93e3a33325afda253d98a15b6c383a5d4
KHANHPHAMTHINGOCKHANH <KHANH.PTN07001@sinhvien.hoasen.edu.vn>
2024-04-16 20:48:29 +0700

    xong location

--- client/src/app/pages/layout/location/detail/detail.component.ts
+++ client/src/app/pages/layout/location/detail/detail.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { LocationState } from '../../../../ngrx/state/location.state';
+import { Location } from '../../../../models/location.model';
+import { ShareModule } from '../../../../shared/shared.module';
+import { TaigaModule } from '../../../../shared/taiga.module';
+import { get } from '../../../../ngrx/actions/location.actions'; 
 @Component({
-  selector: 'app-detail',
+  selector: 'app-detail',  
   standalone: true,
-  imports: [],
+  imports: [ShareModule, TaigaModule],
   templateUrl: './detail.component.html',
-  styleUrl: './detail.component.scss'
+  styleUrls: ['./detail.component.scss']
 })
-export class DetailComponent {
+export class DetailComponent implements OnDestroy {
 
+  location$ = this.store.select('location', 'locationList');
+  locationList: Location[] = [];
+
+  subcriptions: Subscription[] = [];
+  constructor(
+    private router: Router,
+    private store: Store<{ location: LocationState }>
+  ) {
+    this.store.dispatch(get());
+    this.subcriptions.push(
+      this.location$.subscribe((locationList) => {
+        if(locationList.length > 0){
+          console.log(locationList);
+          this.locationList = locationList;
+        }
+      }),
+    );
+  
+  }
+  ngOnInit() {
+    this.store.dispatch(get());
+     this.subcriptions.push(
+       this.location$.subscribe((locationList) => {
+         if(locationList.length > 0){
+           console.log(locationList);
+           this.locationList = locationList;
+         }
+       }),
+     );
+   }
+   ngOnDestroy(): void {
+    this.subcriptions.forEach((sub) => sub.unsubscribe());
+  }
+
+ 
+
+  goBack(): void {
+    this.router.navigate(['/base/location']);
+  }
 }
