97d274c79ab377ff2f45592bdc0300fdcf31a4e0
Hau <hau.bp12201@sinhvien.hoasen.edu.vn>
2024-04-16 17:17:56 +0700

    a

---
+++ client/src/app/models/reservation.model.ts
@@ -0,0 +1,13 @@
+import { Table } from "./table.model";
+import { User } from "./user.model";
+
+export interface Reservation {
+     reservationId: string,
+     numberofPeople: number,
+     date: Date,
+     time: string,
+     tableId: Table,
+     uId: User,
+     phone: string,
+     name: string,
+}
